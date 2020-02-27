7dcb69dbee31b4db6b67220532e1ee90717c0f05
yangpeijian <3571455127@qq.com>
2020-02-27 11:17:30 +0800

    20200227

---
+++ Cache/Backup/config.php
@@ -0,0 +1,11 @@
+<?php
+ return array(
+        'DB_TYPE'=>'mysql',
+        'DB_HOST'=>'localhost',
+        'DB_NAME'=>'huizhuan',
+        'DB_USER'=>'huizhuan',
+        'DB_PWD'=>'huizhuan#321',
+        'DB_PORT'=>'3306',
+        'DB_PREFIX'=>'cosmo_',
+);
+?>
